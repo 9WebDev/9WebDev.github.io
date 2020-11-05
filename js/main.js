32745dfcb12dc8bc8ca850d482e08b5cde72aa15
talant_ <45842773+9WebDev@users.noreply.github.com>
2020-11-05 11:12:57 +0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,40 +1,50 @@
 $(".catalog-ul > li > a").mouseover(function() {
-    if ($(".catalog-ul > li > a").hasClass("active")) {
-        $(".catalog-ul > li > a").removeClass("active");
+    if ($(document).width() >= 1024) {
+        if ($(".catalog-ul > li > a").hasClass("active")) {
+            $(".catalog-ul > li > a").removeClass("active");
+        }
+        $(".subcatalog-ul > li > a").removeClass("active");
+        $(".sidebar-nav .info").removeClass("active");
+        $(this).addClass("active");
     }
-    $(".subcatalog-ul > li > a").removeClass("active");
-    $(".sidebar-nav .info").removeClass("active");
-    $(this).addClass("active");
 });
 $(".subcatalog-ul > li > a").mouseover(function() {
-    if ($(".subcatalog-ul > li > a").hasClass("active")) {
-        $(".subcatalog-ul > li > a").removeClass("active");
+    if ($(document).width() >= 1024) {
+        if ($(".subcatalog-ul > li > a").hasClass("active")) {
+            $(".subcatalog-ul > li > a").removeClass("active");
+        }
+        $(this).addClass("active");
+        $(".sidebar-nav .info").addClass("active")
     }
-    $(this).addClass("active");
-    $(".sidebar-nav .info").addClass("active")
 });
 $(".subcatalog-ul > li > a").mouseout(function() {
     // $(".sidebar-nav .info").removeClass("active");
 });
 $(".sidebar-nav .info a").mouseover(function() {
-    if ($(".sidebar-nav .info a").hasClass("active")) {
-        $(".sidebar-nav .info a").removeClass("active");
+    if ($(document).width() >= 1024) {
+        if ($(".sidebar-nav .info a").hasClass("active")) {
+            $(".sidebar-nav .info a").removeClass("active");
+        }
+        $(this).addClass("active");
     }
-    $(this).addClass("active");
 })
 $(".sidebar-nav .info a").mouseout(function() {
-    if ($(".sidebar-nav .info a").hasClass("active")) {
-        $(".sidebar-nav .info a").removeClass("active");
+    if ($(document).width() >= 1024) {
+        if ($(".sidebar-nav .info a").hasClass("active")) {
+            $(".sidebar-nav .info a").removeClass("active");
+        }
     }
 })
 
 $(document).on("click", function(e) {
-    var listOfLinks = $(".sidebar-nav a");
-    listOfLinks.each(function(item) {
-        if (e.target != listOfLinks[item]){
-            $(".sidebar-nav a, .sidebar-nav .info").removeClass("active");
-        }
-    })
+    if ($(document).width() >= 1024) {
+        var listOfLinks = $(".sidebar-nav a");
+        listOfLinks.each(function(item) {
+            if (e.target != listOfLinks[item]){
+                $(".sidebar-nav a, .sidebar-nav .info").removeClass("active");
+            }
+        })
+    }
 })
 
 // Searchbar
